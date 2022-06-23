5922418a1d9c041eab79eb51c0b4a24f93c20a11
hailey <yanghyeri73@gmail.com>
2022-06-24 08:54:33 +0900

    final work

---
+++ public/js/custom-swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mainSwiper", {
+    slidesPerView: 1.1,
+    spaceBetween: 5,
+    centeredSlides: true,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: false,
+});
+var swiper = new Swiper(".aboutSwiper", {
+    slidesPerView: 1.1,
+    spaceBetween: 5,
+    centeredSlides: true,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: false,
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,22 @@
+// HEADER STICKY
+window.onscroll = function () {
+    headerSticky()
+    
+};
+window.onload = function () {
+    headerSticky()
+};
+
+const header = document.querySelector('header')
+var sticky = header.offsetTop;
+function headerSticky() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("header-sticky");
+    } else {
+        header.classList.remove("header-sticky");
+    }
+}
+
 // SELECT ALL
 function selectAll(selectAll)  {
     const checkboxes = document.getElementsByName('agree-post');
@@ -10,19 +29,84 @@ function selectAll(selectAll)  {
 
 // MODAL
 function modalClose(event){
-    console.log('ho')
     let target = event.target;
     let modal = target.closest('.modal')
     modal.classList.remove('.show')
 
 }
-function test(){
-    alert('hi')
+
+// ACCORDIAN
+const accordianItemHeaders = document.querySelectorAll(
+    ".accordian-item-header"
+);
+
+accordianItemHeaders.forEach(accordianItemHeader => {
+    accordianItemHeader.addEventListener("click", () => {
+        const current = document.querySelector(".accordian-item-header.active");
+
+        if (current && current !== accordianItemHeader) {
+            current.classList.toggle("active");
+            current.nextElementSibling.style.maxHeight = 0;
+        }
+        accordianItemHeader.classList.toggle("active");
+
+        const accordianItemBody = accordianItemHeader.nextElementSibling;
+
+        if (accordianItemHeader.classList.contains("active")) {
+            accordianItemBody.style.maxHeight = accordianItemBody.scrollHeight + "px";
+        } else {
+            accordianItemBody.style.maxHeight = 0;
+        }
+    });
+});
+
+// ROLLING
+jQuery(document).ready(function ($) {
+    $('.my-news-ticker').AcmeTicker({
+        type:'horizontal',
+        direction: 'right',
+        
+    });
+})
+
+//TAB MENU VISIBILITY EVENT -start
+const tabContents = document.querySelectorAll('.tab-content')
+const tabLists = document.querySelectorAll('.tab-list')
+
+tabLists.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+      hideAllContents()
+      hideAllItems()
+      item.classList.add('on')
+      tabContents[idx].classList.add('on')
+    })
+
+})
+
+function hideAllContents() {
+    tabContents.forEach(tabContent => tabContent.classList.remove('on'))
 }
-// $("div[id^='modal-agree']").each(function () {
-//     $(' .close, .btn-confirm, .btn-default .modal-backdrop, .modal-new').click(function () {
-//         $('.modal').removeClass('show');
-//         $('.modal').css('display', 'none');
-//         $('.modal-backdrop').removeClass('show');
-//     });
-// });
+
+function hideAllItems() {
+    tabLists.forEach(tabList => tabList.classList.remove('on'))
+}
+
+// CALENDAR
+$( function() {
+    $( "#datepicker" ).datepicker({
+        dayNames: ['SUN', 'MON', 'TUE','WED','THU','FRI','SAT'],
+        dayNamesMin: ['SUN', 'MON', 'TUE','WED','THU','FRI','SAT'],
+        monthNames: ['1','2','3','4','5','6','7','8','9','10','11','12']
+    })
+} );
+
+
+// LOADING ANIMATION JS
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('.loading-wrap').classList.add('on')
+    document.querySelector('body').classList.add('loading')
+});
+window.addEventListener("load", function(){
+    document.querySelector('.loading-wrap').classList.remove('on')
+    document.querySelector('body').classList.remove('loading')
+});
